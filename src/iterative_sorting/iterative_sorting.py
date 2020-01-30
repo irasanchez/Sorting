a63c9eaa4db8fae8739ddbe6356d01f6562c0d55
Ira <ira.d.sanchez@gmail.com>
2020-01-29 22:34:16 -0700

    Add minor changes to print statements

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,33 +3,24 @@ def selection_sort(arr):
     print(arr)
     print("Selection sort begins")
     # For all indices EXCEPT the last index:
-    print("\n\tinitiate 1st loop")
+    print("\n\tinitiate 1st loop to sort each item")
     for i in range(0, len(arr) - 1):
         print(arr)
-        # a. Loop through elements on right-hand-side
-        # of current index and find the smallest element
         print(f"\tSetting current index to {i} where the value is {arr[i]}")
-        cur_index = i  # starts at 0
+        cur_index = i
         print(
             f"\tSetting smallest index to {cur_index} where the value is {arr[cur_index]}")
-        smallest_index = cur_index  # this value will change
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # for Loop range(cur_index, length)
-        # what is length? the whole list - 1 again?
+        smallest_index = cur_index
         for j in range(cur_index, len(arr)-1):
-            print("\n\t\tInitiate nested loop")
-            # if the nested index is less than the smallest index
+            print("\n\t\tInitiate nested loop to go find next smallest index")
             print(f"\t\tSetting nested index to {j}")
             nested_index = j
             if arr[nested_index] < arr[smallest_index]:
                 print(
                     f"\t\tNested index's value {arr[nested_index]} IS smaller than smallest index's value {arr[smallest_index]}")
-                # smallest_index = nested loop's current index not line 7 index
                 smallest_index = nested_index
                 print(
                     f"\n\t\t!! Resetting smallest index to {nested_index} !!")
-                # array[0] , array[1] = array[1] , array[0]
                 print("\t\t\t!!Swapping!!")
                 arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
                 print(
@@ -37,10 +28,6 @@ def selection_sort(arr):
             else:
                 print(
                     f"\t\tNested index's value {arr[nested_index]} IS NOT smaller than smallest index's value {arr[smallest_index]}")
-
-        # TO-DO: swap
-        # b. Swap the element at current index with the
-        # smallest element found in above loop
     return arr
 
 
