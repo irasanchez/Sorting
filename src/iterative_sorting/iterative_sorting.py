ff597214a3e772c20150894fef586cd2caf78ac6
Ira <ira.d.sanchez@gmail.com>
2020-01-31 20:28:10 -0700

    Add Fixed Selection_Sort
    - I forgot to run the test file before
    - There are notes included for bubble_sort as well

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,35 +1,35 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # For all indices EXCEPT the last index:
     for i in range(0, len(arr)-1):
         cur_index = i
         smallest_index = cur_index
-        for j in range(cur_index, len(arr)-1):
-            nested_index = j
-            if arr[nested_index] < arr[smallest_index]:
 
-                smallest_index = nested_index
+        for j in range(cur_index, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
-                arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
-
-            else:
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
 
     return arr
-
-
 # TO-DO:  implement the Bubble Sort function below
-# def bubble_sort(arr):
-#     #     1. Loop through your array
 
-#     for i in arr:
-#         #     - Compare each element to its neighbor
-#         #     - If elements in wrong position (relative to each other,
-#             # swap them)
-#         # 2. If no swaps performed, stop.
-#         # Else,
-#             #   go back to the element at index 0 and repeat step 1.
 
-#     return arr
+def bubble_sort(arr):
+    pass
+    # swap_flag = True
+
+    # while swap_flag == True:
+    #     swap_flag = False
+
+    #     for i in arr:
+    #         # compare to item on RHS
+    #         rhs = i+1
+    #         if arr[i] > arr[rhs]:
+    #             temp = arr[i]
+    #             arr[i] = arr[rhs]
+    #             arr[rhs] = temp
 
 
 # STRETCH: implement the Count Sort function below
