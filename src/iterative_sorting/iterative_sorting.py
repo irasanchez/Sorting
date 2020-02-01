f1709205ef745ff74ccbf089fecb5b2ad9b71bd8
Ira <ira.d.sanchez@gmail.com>
2020-01-31 20:41:42 -0700

    Add Bubble Sort Implementation
    💪💪💪💪💪

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,19 +17,21 @@ def selection_sort(arr):
 
 
 def bubble_sort(arr):
-    pass
-    # swap_flag = True
+    swap_flag = True
 
-    # while swap_flag == True:
-    #     swap_flag = False
+    while swap_flag == True:
+        swap_flag = False
 
-    #     for i in arr:
-    #         # compare to item on RHS
-    #         rhs = i+1
-    #         if arr[i] > arr[rhs]:
-    #             temp = arr[i]
-    #             arr[i] = arr[rhs]
-    #             arr[rhs] = temp
+        for i in range(0, len(arr)-1):
+            # compare to item on RHS
+            rhs = i+1
+            if arr[i] > arr[rhs]:
+                temp = arr[i]
+                arr[i] = arr[rhs]
+                arr[rhs] = temp
+                swap_flag = True
+
+    return arr
 
 
 # STRETCH: implement the Count Sort function below
