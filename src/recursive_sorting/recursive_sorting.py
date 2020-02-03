7e2c9d2add16aee955580b226991873e5fd887dd
Ira <ira.d.sanchez@gmail.com>
2020-02-02 21:15:57 -0700

    Began implementing
    - Blocked by the merge() helper function
    - Trying to get the two arguments it needs
    while still having a base case of 1
    - Currently broken with endless recursion

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,26 +1,56 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
+    print("elements: ", elements)
     merged_arr = [0] * elements
     # TO-DO
-    
+    if arrA[0] < arrB[0]:
+        merged_arr[0] = arrA[0]
+    else:
+        merged_arr[0] = arrB[0]
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+# 1. While your data set contains more than one item, split it in half
+# 2. Once you have gotten down to a single element, you have also *sorted* that element
+#    (a single element cannot be "out of order")
+# 3. Start merging your single lists of one element together into larger, sorted sets
+# 4. Repeat step 3 until the entire data set has been reassembled
+
+def merge_sort(arr):
+    print("merge sort called for ", arr)
+    middle = int(len(arr) / 2)
+    print("middle index: ", middle)
+    first_half = arr[:middle]
+    second_half = arr[middle:]
+
+    if len(first_half) == 1 and len(second_half) == 1:  # base
+        # :'s send data as list, not just element
+
+        print(merge(first_half, second_half))
+
+    else:  # recursive case
+        print("merge sort first half")
+        merge_sort(first_half)  # recurse first half
+        print("merge sort second half")
+        merge_sort(second_half)  # recurse second half
 
     return arr
 
+
+merge_sort([3, 2, 1, 6, 4, 5, 8])
 
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
-    # TO-DO
+        # TO-DO
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +58,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
