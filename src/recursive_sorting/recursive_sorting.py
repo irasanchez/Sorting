ab7c97f0c0776930123df757e32c123b79d14f43
Ira <ira.d.sanchez@gmail.com>
2020-02-03 21:55:50 -0700

    Add Complete Merge Sort w Merge in Planning Phase

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -4,37 +4,27 @@ def merge(arrA, arrB):
     print("elements: ", elements)
     merged_arr = [0] * elements
     # TO-DO
-    if arrA[0] < arrB[0]:
-        merged_arr[0] = arrA[0]
-    else:
-        merged_arr[0] = arrB[0]
-    return merged_arr
+    for i in range(merged_arr):
+        if len(arrA) == 0:
+            # copy rhs to merged_arr
+            # merged_arr[i:] = arrB[]
+            # if rhs is empty
+        if len(arrB) == 0:
+            pass
+            # copy lhs to merged_arr
+        # arrA[0] < arrB[0]:
+        merged_arr[i] = arrA[0]
+        arrA.pop(0)
+        # arrB[0] < arrA[0]
+        merged_arr[i] = arrB[0]
+        arrB.pop(0)
 
-
-# TO-DO: implement the Merge Sort function below USING RECURSION
-# 1. While your data set contains more than one item, split it in half
-# 2. Once you have gotten down to a single element, you have also *sorted* that element
-#    (a single element cannot be "out of order")
-# 3. Start merging your single lists of one element together into larger, sorted sets
-# 4. Repeat step 3 until the entire data set has been reassembled
 
 def merge_sort(arr):
-    print("merge sort called for ", arr)
-    middle = int(len(arr) / 2)
-    print("middle index: ", middle)
-    first_half = arr[:middle]
-    second_half = arr[middle:]
-
-    if len(first_half) == 1 and len(second_half) == 1:  # base
-        # :'s send data as list, not just element
-
-        print(merge(first_half, second_half))
-
-    else:  # recursive case
-        print("merge sort first half")
-        merge_sort(first_half)  # recurse first half
-        print("merge sort second half")
-        merge_sort(second_half)  # recurse second half
+    if len(arr) > 1:
+        lhs = merge_sort(arr[:len(arr)//2])
+        rhs = merge_sort(arr[len(arr)//2:])
+        merge(lhs, rhs)
 
     return arr
 
